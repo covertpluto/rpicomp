1f92e1e0cbd158b75f08892cbd4ba09e7b13bbcb
alex <covertpluto3502@gmail.com>
2022-03-10 19:02:33 +0000

    small changes

---
+++ copy_and_setup.sh
@@ -0,0 +1,9 @@
+#sudo apt update
+#sudo apt full-upgrade
+#sudo apt install python3
+#sudo apt install python3
+sudo rm -r /etc/thebox/ # deleting the old version in case an update was issued
+sudo mkdir /etc/thebox/
+sudo git clone "https://github.com/covertpluto3502/rpicomp.git" "/etc/thebox/"
+
+
