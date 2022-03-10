fe07d114021dca4c25295a7373ca53cfb968e5ad
alex <covertpluto3502@gmail.com>
2022-03-10 18:40:38 +0000

    small changes

---
+++ sensors.py
@@ -0,0 +1,11 @@
+import Adafruit_DHT
+
+
+
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+
+def temperature():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    return humidity, temperature
